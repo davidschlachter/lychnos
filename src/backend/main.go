907731315eb38b0ce17a01b9cc1814a413563ea0
David Schlachter <github@schlachter.ca>
2021-12-28 23:34:16 -0500

    Add category summary report

--- src/backend/main.go
+++ src/backend/main.go
@@ -10,13 +10,14 @@ import (
 	"github.com/davidschlachter/lychnos/src/backend/budget"
 	"github.com/davidschlachter/lychnos/src/backend/categorybudget"
 	"github.com/davidschlachter/lychnos/src/backend/firefly"
+	"github.com/davidschlachter/lychnos/src/backend/report"
 )
 
 func main() {
 	db := connect()
 	setupDB(db)
 
-	_, err := firefly.New(&http.Client{Timeout: time.Second * 5}, os.Getenv("FIREFLY_TOKEN"), os.Getenv("FIREFLY_URL"))
+	f, err := firefly.New(&http.Client{Timeout: time.Second * 5}, os.Getenv("FIREFLY_TOKEN"), os.Getenv("FIREFLY_URL"))
 	if err != nil {
 		fmt.Printf("Could not initialize Firefly-III client: %s\n", err)
 		os.Exit(1)
@@ -27,6 +28,13 @@ func main() {
 
 	c := categorybudget.New(db)
 	http.HandleFunc("/api/categorybudgets/", c.Handle)
+
+	r, err := report.New(f, c, b)
+	if err != nil {
+		fmt.Printf("Could not initialize reports: %s\n", err)
+		os.Exit(1)
+	}
+	http.HandleFunc("/api/reports/", r.Handle)
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "ok\n")
