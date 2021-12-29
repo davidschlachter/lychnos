c6c80d3de2dfafce068aaf3aa7f5c8845f94d7e1
David Schlachter <github@schlachter.ca>
2021-12-29 16:30:15 -0500

    Add fetch for CategoryTotal

--- src/backend/firefly/firefly.go
+++ src/backend/firefly/firefly.go
@@ -88,7 +88,7 @@ type rawTotal struct {
 	Sum string `json:"sum"`
 }
 
-func (f *Firefly) CategoryTotals(start, end time.Time) ([]CategoryTotal, error) {
+func (f *Firefly) ListCategoryTotals(start, end time.Time) ([]CategoryTotal, error) {
 	const path = "/api/v1/categories/"
 	params := fmt.Sprintf("?start=%s&end=%s", start.Format("2006-01-02"), end.Format("2006-01-02"))
 
@@ -140,6 +140,60 @@ func (f *Firefly) CategoryTotals(start, end time.Time) ([]CategoryTotal, error) 
 
 		results = append(results, c)
 	}
+
+	return results, nil
+}
+
+func (f *Firefly) FetchCategoryTotal(catID int, start, end time.Time) ([]CategoryTotal, error) {
+	const path = "/api/v1/categories/"
+	params := fmt.Sprintf("?start=%s&end=%s", start.Format("2006-01-02"), end.Format("2006-01-02"))
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s%s%d%s", f.url, path, catID, params), nil)
+	req.Header.Add("Authorization", "Bearer "+f.token)
+	resp, err := f.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch Category: %s", err)
+	}
+	defer resp.Body.Close()
+
+	var rawResults struct {
+		Data rawCategoryTotal `json:"data"`
+	}
+	json.NewDecoder(resp.Body).Decode(&rawResults)
+
+	var results []CategoryTotal
+
+	r := &rawResults.Data
+	if len(r.Attributes.Spent) == 0 && len(r.Attributes.Earned) == 0 {
+		return nil, fmt.Errorf("no earned or spent for category")
+	}
+	var c CategoryTotal
+	var spent, earned decimal.Decimal
+	id, err := strconv.Atoi(r.ID)
+	if err != nil {
+		return nil, fmt.Errorf("could not convert id to int: %s", err)
+	}
+	if len(r.Attributes.Spent) == 1 {
+		spent, err = decimal.NewFromString(r.Attributes.Spent[0].Sum)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert spent sum to decimal: %s", err)
+		}
+	}
+	if len(r.Attributes.Earned) == 1 {
+		earned, err = decimal.NewFromString(r.Attributes.Earned[0].Sum)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert earned sum to decimal: %s", err)
+		}
+	}
+
+	c.ID = id
+	c.Name = r.Attributes.Name
+	c.Spent = spent
+	c.Earned = earned
+	c.Start = start
+	c.End = end
+
+	results = append(results, c)
 
 	return results, nil
 }
--- src/backend/firefly/firefly_test.go
+++ src/backend/firefly/firefly_test.go
@@ -30,6 +30,9 @@ func TestMain(m *testing.M) {
 
 func setup() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/categories/4", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"data":{"type":"categories","id":"4","attributes":{"created_at":"2019-09-07T20:02:33-04:00","updated_at":"2019-09-07T20:02:33-04:00","name":"Apartment","notes":null,"spent":[{"sum":"-323.75","currency_id":9,"currency_name":"Canadian dollar","currency_symbol":"C$","currency_code":"CAD","currency_decimal_places":2}],"earned":[{"sum":"54.23","currency_id":9,"currency_name":"Canadian dollar","currency_symbol":"C$","currency_code":"CAD","currency_decimal_places":2}]},"links":{"0":{"rel":"self","uri":"/categories/4"},"self":"http://192.168.6.4:8753/api/v1/categories/4"}}}`))
+	})
 	mux.HandleFunc("/api/v1/categories/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`{"data":[{"type":"categories","id":"4","attributes":{"created_at":"2019-09-07T20:02:33-04:00","updated_at":"2019-09-07T20:02:33-04:00","name":"Apartment","notes":null,"spent":[{"sum":"-237.80","currency_id":9,"currency_name":"Canadian dollar","currency_symbol":"C$","currency_code":"CAD","currency_decimal_places":2}],"earned":[{"sum":"54.23","currency_id":9,"currency_name":"Canadian dollar","currency_symbol":"C$","currency_code":"CAD","currency_decimal_places":2}]},"links":{"0":{"rel":"self","uri":"/categories/4"},"self":"http://lychnos/api/v1/categories/4"}}]}`))
 	})
@@ -55,11 +58,11 @@ func TestCategories(t *testing.T) {
 	}
 }
 
-func TestCategoryTotals(t *testing.T) {
+func TestListCategoryTotals(t *testing.T) {
 	// (Interval not considered in test)
 	start := time.Now().Add(time.Hour * -1)
 	end := time.Now()
-	c, err := f.CategoryTotals(start, end)
+	c, err := f.ListCategoryTotals(start, end)
 	if err != nil {
 		t.Fatalf("Unexpected error: %s\n", err)
 	}
@@ -75,6 +78,39 @@ func TestCategoryTotals(t *testing.T) {
 	spent, _ := decimal.NewFromString("-237.80")
 	if !c[0].Spent.Equal(spent) {
 		t.Fatalf("Got %d as Spent, wanted -237.80", c[0].Spent)
+	}
+	earned, _ := decimal.NewFromString("54.23")
+	if !c[0].Earned.Equal(earned) {
+		t.Fatalf("Got %d as Earned, wanted 54.23", c[0].Earned)
+	}
+	if !c[0].Start.Equal(start) {
+		t.Fatalf("Got %s as Start, wanted %s", c[0].Start, start)
+	}
+	if !c[0].End.Equal(end) {
+		t.Fatalf("Got %s as End, wanted %s", c[0].End, end)
+	}
+}
+
+func TestFetchCategoryTotal(t *testing.T) {
+	// (Interval not considered in test)
+	start := time.Now().Add(time.Hour * -1)
+	end := time.Now()
+	c, err := f.FetchCategoryTotal(4, start, end)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s\n", err)
+	}
+	if len(c) != 1 {
+		t.Fatalf("Got %d CategoryTotals, wanted 1", len(c))
+	}
+	if c[0].Name != "Apartment" {
+		t.Fatalf("Got %s as Category name, wanted Apartment", c[0].Name)
+	}
+	if c[0].ID != 4 {
+		t.Fatalf("Got %d as Category ID, wanted 4", c[0].ID)
+	}
+	spent, _ := decimal.NewFromString("-323.75")
+	if !c[0].Spent.Equal(spent) {
+		t.Fatalf("Got %d as Spent, wanted -323.75", c[0].Spent)
 	}
 	earned, _ := decimal.NewFromString("54.23")
 	if !c[0].Earned.Equal(earned) {
