b77ed83e4af6cb1eb10990030fb0cdf85b529395
David Schlachter <davidschlachter@users.noreply.github.com>
2022-09-05 20:29:55 -0400

    Add support for SQLite (#37)
    Implement support for SQLite, along-side MySQL/MariaDB. While an existing
    Firefly-III installation is likely to be running on MySQL, SQLite is a simpler
    and lighter alternative if lychnos is running on a different host. Since we're
    also automatically creating the database file if none is specified, this
    simplifies setup for new users. Eventually, I would like to be able to
    distribute lychnos in a Docker image, and removing the dependency on MySQL is a
    first step to accomplish this.
    
    Caveat: personally I'm using MySQL, so SQLite is not yet thoroughly tested.

--- src/backend/db.go
+++ src/backend/db.go
@@ -7,12 +7,51 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func connect() *sql.DB {
-	db, err := sql.Open("mysql", os.Getenv("DSN"))
+type connectionType int
+
+const (
+	connectionTypeSQLite connectionType = iota
+	connectionTypeMySQL
+)
+
+var connectionTypeStrings = [...]string{
+	connectionTypeSQLite: "sqlite",
+	connectionTypeMySQL:  "mysql",
+}
+
+func (c connectionType) String() string {
+	if int(c) < 0 || int(c) >= len(connectionTypeStrings) {
+		return ""
+	}
+	return connectionTypeStrings[int(c)]
+}
+
+type connectionString struct {
+	DSN  string
+	Type connectionType
+}
+
+// connect connects to the database, based on the connection string provided by
+// the user. If no connection string was provided, attempt to create a SQLite
+// database.
+func connect() (*sql.DB, error) {
+	var db *sql.DB
+
+	dsn, err := getDSN()
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+
+	if dsn.DSN == "" {
+		db, err = createSQLite()
+	} else {
+		db, err = sql.Open(dsn.Type.String(), dsn.DSN)
+	}
+	if err != nil {
+		return nil, err
 	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -22,17 +61,57 @@ func connect() *sql.DB {
 	// Validate database connection
 	err = db.Ping()
 	if err != nil {
-		fmt.Printf("Could not connect to database: %s", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("could not connect to database: %s", err)
 	}
 
-	return db
+	// Set up tables
+	err = setupDB(&dsn, db)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize tables: %s", err)
+	}
+
+	return db, nil
+}
+
+func createSQLite() (*sql.DB, error) {
+	// create the DB
+	db, err := sql.Open("sqlite3", "./database.sqlite3")
+	return db, err
+}
+
+// getDSN reads the database connection string from the environment, returning
+// it along with its type.
+func getDSN() (connectionString, error) {
+	var (
+		dsn                 connectionString
+		mysqlDSN, sqliteDSN string
+	)
+	mysqlDSN = os.Getenv("MYSQL_DSN")
+	sqliteDSN = os.Getenv("SQLITE_DSN")
+
+	if mysqlDSN != "" && sqliteDSN != "" {
+		return dsn, fmt.Errorf("MYSQL_DSN and SQLITE_DSN cannot both be set")
+	}
+
+	if mysqlDSN != "" {
+		dsn.DSN = mysqlDSN
+		dsn.Type = connectionTypeMySQL
+	} else {
+		dsn.DSN = sqliteDSN // okay if empty, will create SQLite database
+		dsn.Type = connectionTypeSQLite
+	}
+
+	return dsn, nil
 }
 
 // setupDB will create the tables that lychnos uses to store its budget data, if
 // they do not already exist.
-func setupDB(db *sql.DB) {
-	q := []string{`
+func setupDB(dsn *connectionString, db *sql.DB) error {
+	var q []string
+
+	if dsn != nil && dsn.Type == connectionTypeMySQL {
+		// MySQL
+		q = []string{`
 CREATE TABLE IF NOT EXISTS budgets (
 	id INT NOT NULL AUTO_INCREMENT,
 	start DATETIME NOT NULL,
@@ -50,12 +129,31 @@ CREATE TABLE IF NOT EXISTS category_budgets (
 	FOREIGN KEY ( budget ) REFERENCES budgets( id )
 );
 `}
+	} else {
+		// SQLite
+		q = []string{`
+CREATE TABLE IF NOT EXISTS budgets (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	start DATETIME NOT NULL,
+	end DATETIME NOT NULL,
+	reporting_interval INT NOT NULL
+);
+`, `
+CREATE TABLE IF NOT EXISTS category_budgets (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	budget INT,
+	category INT,
+	amount DECIMAL(12,4),
+	FOREIGN KEY ( budget ) REFERENCES budgets( id )
+);
+		`}
+	}
 
 	for _, s := range q {
 		_, err := db.Exec(s)
 		if err != nil {
-			fmt.Printf("Could not initialize database tables: %s", err)
-			os.Exit(1)
+			return err
 		}
 	}
+	return nil
 }
