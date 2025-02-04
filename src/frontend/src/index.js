e665cbf27eb01e3675fad580873f1ea8de4397f0
David Schlachter <github@schlachter.ca>
2025-02-04 10:58:14 -0500

    Update to React 19

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,11 +1,11 @@
+import { createRoot } from "react-dom/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
