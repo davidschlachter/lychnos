400db4bc572f56a637d819eb1ec3f0a8769ceb9c
David Schlachter <t480-debian-git@schlachter.ca>
2025-12-23 23:37:18 -0500

    Fix non-existent property

--- src/frontend/src/CategorySummaries.js
+++ src/frontend/src/CategorySummaries.js
@@ -128,7 +128,7 @@ class CategorySummaries extends React.Component {
                 <>
                     <Header back_visibility="hidden" title="Category summaries" budgetedit={true}></Header>
                     <Box sx={{ p: 2, pb: 8 }}>
-                        <TableContainer component={Paper} style={{ "overflow-y": "hidden" }}>
+                        <TableContainer component={Paper} style={{ "overflowY": "hidden" }}>
                             <Table style={{ "width": "100%" }}>
                                 <TableHead>
                                     <TableRow>
